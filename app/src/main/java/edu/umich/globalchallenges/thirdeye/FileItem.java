16aefe46fc666a62d3c6dd57d51e0f567e82ef7c
Matt Kohls <mattkohls13@gmail.com>
2018-07-11 14:59:49 -0400

    Switching adapter to FlexibleAdapter
    So far we are able to use that to display list items

---
+++ app/src/main/java/edu/umich/globalchallenges/thirdeye/FileItem.java
@@ -0,0 +1,63 @@
+package edu.umich.globalchallenges.thirdeye;
+
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem;
+import eu.davidea.flexibleadapter.items.IFlexible;
+import eu.davidea.viewholders.FlexibleViewHolder;
+
+public class FileItem  extends AbstractFlexibleItem<FileItem.FileViewHolder> {
+
+    private String filename;
+    private String date;
+
+    public FileItem(String date, String filename) {
+        this.date = date;
+        this.filename = filename;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object instanceof FileItem) {
+            return this.date.equals(((FileItem) object).date) && this.filename.equals(((FileItem) object).filename);
+        }
+        return false;
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.text_view;
+    }
+
+    @Override
+    public FileViewHolder createViewHolder(View view, FlexibleAdapter<IFlexible> adapter) {
+        return new FileViewHolder(view, adapter);
+    }
+
+    @Override
+    public void bindViewHolder(FlexibleAdapter<IFlexible> adapter, FileViewHolder holder, int position, List<Object> payloads) {
+        holder.date.setText(date);
+        holder.filename.setText(filename);
+        holder.date.setEnabled(isEnabled());
+        holder.filename.setEnabled(isEnabled());
+    }
+
+    /**
+     * Class defining the how the items will be viewed
+     */
+    public class FileViewHolder extends FlexibleViewHolder {
+
+        public TextView date;
+        public TextView filename;
+
+        public FileViewHolder(View view, FlexibleAdapter adapter) {
+            super(view, adapter);
+            this.date = (TextView) view.findViewById(R.id.date);
+            this.filename = (TextView) view.findViewById(R.id.filename);
+        }
+    }
+}
--- app/src/main/java/edu/umich/globalchallenges/thirdeye/FileViewer.java
+++ app/src/main/java/edu/umich/globalchallenges/thirdeye/FileViewer.java
@@ -10,26 +10,44 @@ import android.support.v7.widget.DividerItemDecoration;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.flexibleadapter.items.IFlexible;
+
 public class FileViewer extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    //private RecyclerView.Adapter adapter;
+    private FlexibleAdapter<IFlexible> adapter;
     private LinearLayoutManager layoutManager;
+
+    private List<IFlexible> database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_viewer);
+
+        database = new ArrayList<>();
+        database.add(new FileItem("Nothing here yet!", "Loading..."));
+
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
         recyclerView.setHasFixedSize(true);
         // Set the layout we want for the list
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
         // Load in data
         RequestQueue queue = Volley.newRequestQueue(this);
+        /*
         adapter = new JsonAdapter(queue);
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new DividerItemDecoration(this,
                 layoutManager.getOrientation()));
+        */
+        adapter = new FlexibleAdapter<>(database);
+        recyclerView.setAdapter(adapter);
     }
 }
