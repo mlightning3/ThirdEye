56a9a728264c35a8774a83d1e83c8f203e35d34a
Matt Kohls <mattkohls13@gmail.com>
2018-07-31 14:49:28 -0400

    Cleaning up files a bit

--- app/src/main/java/edu/umich/globalchallenges/thirdeye/FileViewerFragment.java
+++ app/src/main/java/edu/umich/globalchallenges/thirdeye/FileViewerFragment.java
@@ -20,7 +20,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
-import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -29,7 +28,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
